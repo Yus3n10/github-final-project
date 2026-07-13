0ae4599a44ca05f5e51ac486058cef5741088ef7
Ptheusen Geagoni <pgeagoni@gmail.com>
2026-07-13 21:27:57 +0800

    Implement simple interest calculation script
    Added a script to calculate simple interest based on user input.

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,2 +1,17 @@
-chmod +x simple-interest.sh
-./simple-interest.sh
+#!/bin/bash
+
+# This script calculates simple interest.
+# Simple Interest = (Principal * Rate * Time) / 100
+
+echo "Enter the Principal:"
+read principal
+
+echo "Enter the Rate of Interest:"
+read rate
+
+echo "Enter the Time period (in years):"
+read time
+
+interest=$(echo "$principal * $rate * $time / 100" | bc)
+
+echo "The Simple Interest is: $interest"
